2d7fdc2d8d364d42244a508c11dea7951137be99
Oliver <oliver@kinson.cloud>
2025-03-21 22:56:50 -0600

    prepaired for github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: "https://demo.site",
+    site: "https://foss.cooking",
     // reidrects abc.xyz/ -> abc.xyz
     trailingSlash: 'never'
 });
